0344ce1066f01ee7c4729715dc915506dd2a33a2
Ilhampratama08 <ilham.pratama2791@smk.belajar.id>
2024-03-26 10:17:45 +0700

    fixbells

--- src/pages/bells.tsx
+++ src/pages/bells.tsx
@@ -17,7 +17,7 @@ const homepage = () => {
             
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/pp.jpg" width={40} height={40}> 
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/pp.jpg" width={40} height={40}> 
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Darwin</b></p>
                    <p className="ms-[-3rem] h-[1rem]">Liked your post</p>
@@ -27,7 +27,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4.5rem] h-[4rem] ms-[-1rem] rounded-full" src="/adul.jpg" height={80} width={80}> 
+                   <Image className="w-[4.5rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/adul.jpg" height={80} width={80}> 
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Darmian</b></p>
                    <p className="ms-[-4rem] h-[1rem]">Started following you</p>
@@ -38,7 +38,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] opacity rounded-full" src="/prel.jpg" width={80} height={80}> 
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] opacity rounded-full" alt="bel" src="/prel.jpg" width={80} height={80}> 
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Wilson</b></p>
                    <p className="ms-[-3rem] h-[1rem]">Started following you</p>
@@ -49,7 +49,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/ern.jpg" width={80} height={80}> 
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/ern.jpg" width={80} height={80}> 
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Erwin</b></p>
                    <p className="ms-[-2rem] h-[1rem]">Started following you</p>
@@ -60,7 +60,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/jrt.jpg" width={80} height={80}>
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/jrt.jpg" width={80} height={80}>
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Jarot</b></p>
                    <p className="ms-[-2rem] h-[1rem]">Started following you</p>
@@ -71,7 +71,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/kepin.jpg" width={80} height={80}>
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/kepin.jpg" width={80} height={80}>
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Kevin</b></p>
                    <p className="ms-[-2rem] h-[1rem]">Started following you</p>
@@ -82,7 +82,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/edg.jpg" width={80} height={80}>
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/edg.jpg" width={80} height={80}>
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Edgar</b></p>
                    <p className="ms-[-2rem] h-[1rem]">Started following you</p>
@@ -93,7 +93,7 @@ const homepage = () => {
             <div className="mt-4 h-[0.5px] bg-black"></div>
             <div className=" mt-2 items-center">
                 <div className="flex items-center">
-                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" src="/erk.jpg" width={80} height={80}>
+                   <Image className="w-[4rem] h-[4rem] ms-[-1rem] rounded-full" alt="bel" src="/erk.jpg" width={80} height={80}>
                    </Image>
                    <p className="ms-3 h-[4rem]"><b>Erika</b></p>
                    <p className="ms-[-2rem] h-[1rem]">Liked your post</p>
